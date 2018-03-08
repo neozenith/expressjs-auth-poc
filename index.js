611466cffa5deba1ff8657a1a1926378c29b8938
Josh Peak <neozenith.dev@gmail.com>
2018-03-08 15:03:03 +1100

    Add static file serving

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('static'));
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
